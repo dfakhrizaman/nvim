fab1e797f0121dcb4099fc0eb41cea94730ab3f8
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-01-01 03:12:57 +0700

    add gitsigns and autopairs

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,24 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local opts = { buffer = bufnr }
+
+        vim.keymap.set("n", "]c", gs.next_hunk, opts)
+        vim.keymap.set("n", "[c", gs.prev_hunk, opts)
+        vim.keymap.set("n", "<leader>gp", gs.preview_hunk, opts)
+        vim.keymap.set("n", "<leader>gb", gs.blame_line, opts)
+      end,
+    })
+  end,
+}
