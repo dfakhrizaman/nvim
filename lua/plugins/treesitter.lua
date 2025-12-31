8de4ec0204ab0a3649182b569df235e5dbe433dc
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-01-01 02:38:20 +0700

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  tag = "v0.10.0",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "python",
+        "html",
+        "css",
+        "json",
+        "markdown",
+        "bash",
+        "go",
+        "clojure",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
