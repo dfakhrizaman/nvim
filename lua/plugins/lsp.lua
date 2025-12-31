88898874594ad3063adb2b949c89cec2659db275
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-01-01 03:08:59 +0700

    add lualine and lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "pyright",
+          "html",
+          "cssls",
+          "jsonls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      -- LSP keymaps on attach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local opts = { buffer = args.buf, noremap = true, silent = true }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        end,
+      })
+
+      -- Configure servers using vim.lsp.config (Neovim 0.11+)
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.config("pyright", {})
+      vim.lsp.config("html", {})
+      vim.lsp.config("cssls", {})
+      vim.lsp.config("jsonls", {})
+
+      -- Enable configured servers
+      vim.lsp.enable({ "lua_ls", "ts_ls", "pyright", "html", "cssls", "jsonls" })
+    end,
+  },
+}
