ec765ecf2eb4d378fdeb707f700112073277e77b
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-01-01 23:59:36 +0700

    add alpha

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,34 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+      dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+      dashboard.button("g", "  Find text", ":Telescope live_grep<CR>"),
+      dashboard.button("c", "  Config", ":e ~/.config/nvim/init.lua<CR>"),
+      dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "  Quit", ":qa<CR>"),
+    }
+
+    dashboard.section.footer.val = "Happy coding!"
+
+    alpha.setup(dashboard.config)
+  end,
+}
