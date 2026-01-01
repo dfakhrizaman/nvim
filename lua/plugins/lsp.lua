ec6ff2e7cddc49312d125a44b819f6599f1dc965
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-01-02 02:36:17 +0700

    fix global vim warning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,7 +47,19 @@ return {
       })
 
       -- Configure servers using vim.lsp.config (Neovim 0.11+)
-      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
       vim.lsp.config("ts_ls", {})
       vim.lsp.config("pyright", {})
       vim.lsp.config("html", {})
